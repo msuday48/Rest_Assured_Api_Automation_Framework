27270d07c9fa0aeb2d86d6b0d1210cb1f8c17036
msuday48 <msuday48@gmail.com>
2025-02-07 09:10:11 +0530

    push code

--- src/main/java/org/Api_Testing/pojos/BookingRespons.java
+++ src/main/java/org/Api_Testing/pojos/BookingRespons.java
@@ -4,7 +4,7 @@ import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
 
 public class BookingRespons {
-
+//
     @SerializedName("bookingid")
     @Expose
     private Integer bookingid;
